f34e38e2ade9c79562d66fcd20ccfc07d8a3994a
Vi1704ca <vica.ti17@gmail.com>
2026-09-17 22:55:12 +0300

    feat: add products endpoints with query and route params

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import moment from 'moment';    
 
-const app = express();
+const app = express()
 
-const PORT = 3000;
-const HOST = 'localhost';
+const HOST = '127.0.0.1'
+const PORT = 3001
 
 app.get('/timestamp', (req, res) => {
   const now = moment();
@@ -13,6 +12,69 @@ app.get('/timestamp', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+const products = [
+    { 
+        id: 1, 
+        name: 'Cheetos', 
+        price: 10.99, 
+        category: 'Corn snack' 
+    },
+    { 
+        id: 2, 
+        name: 'Chupa Chups', 
+        price: 19.99, 
+        category: 'Confectionery' 
+    },
+    { 
+        id: 3, 
+        name: 'Mountain Dew', 
+        price: 5.99, 
+        category: 'Carbonated drink' 
+    },
+    { 
+        id: 4, 
+        name: 'Doritos', 
+        price: 12.99, 
+        category: 'Corn snack' 
+    },
+    { 
+        id: 5, 
+        name: 'Snickers', 
+        price: 15.99, 
+        category: 'Confectionery' 
+    },
+];
+
+app.get('/products', (req, res) => {
+  const { take, category } = req.query;
+  let filteredProducts = products;
+
+  if (category) {
+    filteredProducts = filteredProducts.filter((product) => product.category === category);
+  }
+  if (!take) {
+    return res.status(200).json(filteredProducts);
+  }
+  const takeNum = Number(take);
+
+  if (!Number.isInteger(takeNum) || takeNum < 0) {
+    return res.status(400).json({ message: "take must be a non-negative integer" });
+  }
+  filteredProducts = filteredProducts.slice(0, takeNum);
+  return res.status(200).json(filteredProducts);
+});     
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  const idNum = Number(id);
+  const product = products.find((product) => product.id === idNum);
+
+  if (!product) {
+    return res.status(404).json({ message: "product not found" });
+  }
+  return res.status(200).json(product);
 });
+
+app.listen(PORT, HOST, () => {
+    console.log(`http://${HOST}:${PORT}`)
+})
